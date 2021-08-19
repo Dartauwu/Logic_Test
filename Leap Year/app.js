2e6c19cd393b19da81dd838693f9043a334bae01
Dartauwu <ghoribdart@gmail.com>
2021-08-19 20:48:49 +0700

    first commit

---
+++ Leap Year/app.js
@@ -0,0 +1,36 @@
+function leapyaer(year)
+{
+    return (year % 100 === 0 ) ? (year % 400 === 0) : (year % 4 === 0)
+}
+console.log(leapyaer(1904));
+console.log(leapyaer(1908));
+console.log(leapyaer(1912));
+console.log(leapyaer(1916));
+console.log(leapyaer(1920));
+console.log(leapyaer(1924));
+console.log(leapyaer(1928));
+console.log(leapyaer(1932));
+console.log(leapyaer(1940));
+console.log(leapyaer(1944));
+console.log(leapyaer(1948));
+console.log(leapyaer(1952));
+console.log(leapyaer(1956));
+console.log(leapyaer(1960));
+console.log(leapyaer(1964));
+console.log(leapyaer(1968));
+console.log(leapyaer(1972));
+console.log(leapyaer(1976));
+console.log(leapyaer(1980));
+console.log(leapyaer(1984));
+console.log(leapyaer(1988));
+console.log(leapyaer(1992));
+console.log(leapyaer(1996));
+console.log(leapyaer(2000));
+console.log(leapyaer(2004));
+console.log(leapyaer(2008));
+console.log(leapyaer(2012));
+console.log(leapyaer(2016));
+console.log(leapyaer(2020));
+
+
+
---
+++ Palindrome/app.js
@@ -0,0 +1,28 @@
+let word1 = 'Radar';
+let word2 = 'Malam';
+let word3 = 'Kasur ini rusak';
+let word4 = 'Ibu Ratna antar ubi ';
+let word5 = 'Malas  ';
+let word6 = 'Makan nasi goreng ';
+let word7 = 'Balonku ada lima ';
+
+function checkPalindrome(input){
+    let strArray = input.toLowerCase().split();
+    let newArr = strArray.join('');
+    let reverseArr = [...newArr].reverse().join('');
+
+
+    if(newArr === reverseArr){
+        return true;
+    }else {
+        return false;
+    }
+};
+
+console.log( checkPalindrome(word1));
+console.log( checkPalindrome(word2));
+console.log( checkPalindrome(word3));
+console.log( checkPalindrome(word4));
+console.log( checkPalindrome(word5));
+console.log( checkPalindrome(word6));
+console.log( checkPalindrome(word7));
